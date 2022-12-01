bc1732d44c30a3c8c97cc65b62f2590999410f34
gavvahar <nihargavva@gmail.com>
2022-11-30 21:58:26 -0500

    printing numbers from 1 to 8

--- solution2.py
+++ solution2.py
@@ -1,1 +1,11 @@
 # Write code for algorithm 2 below
+def numbers(low, high):
+    if low > high:
+        return
+    else:
+        print(low)
+        numbers(low + 1, high)
+
+
+i = 8
+numbers(1, i)
