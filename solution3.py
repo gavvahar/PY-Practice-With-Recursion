a7036e81adb4ac488e2ee7a7efed09105a7862cf
gavvahar <nihargavva@gmail.com>
2022-11-30 22:09:06 -0500

    fibonacci

--- solution3.py
+++ solution3.py
@@ -1,1 +1,11 @@
 # Write code for algorithm 3 below
+def fibonacci(number):
+    if number <= 1:
+        return number
+    else:
+        return (fibonacci(number - 1) + fibonacci(number - 2))
+
+
+number = 10
+for i in range(number):
+    print(fibonacci(i))
