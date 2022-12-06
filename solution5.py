1e6b1c18dbfc154cda1786b639b2dbcfd14b8705
gavvahar <nihargavva@gmail.com>
2022-12-05 19:46:57 -0500

    palindrome

--- solution5.py
+++ solution5.py
@@ -1,1 +1,12 @@
 # Write code for algorithm 5 below
+def palindrome(word):
+    if len(word) < 2:
+        return True
+    else:
+        if word[0] == word[-1]:
+            return palindrome(word[1:-1])
+        else:
+            return False
+
+
+print(palindrome("radar"))
